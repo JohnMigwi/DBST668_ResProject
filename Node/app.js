eab66c60ee367f2e17e45b08f45ca84237d522ec
JohnMigwi <johnmigwi101@gmail.com>
2024-06-20 13:17:18 +0300

    routes

--- Node/app.js
+++ Node/app.js
@@ -6,7 +6,10 @@ const {products}= require ('../node-express-course/02-express-tutorial/data')
 const app = express()
 const PORT = 5000;
 
+app.use(express.json());
+
 app.get('/api/AllProducts', (req, res)=>{
+    
     const simplyproducts = products.map(product =>({
         id : product.id,
         name: product.name,
@@ -21,12 +24,13 @@ app.get('/api/singleProduct/:id', (req,res) =>{
         const productID = parseInt(req.params.id)
         const singleProduct = products.find(product=> product.id === productID)
         res.json(singleProduct)
+        console.log(req.body);
 });
 
 
 app.post('/api/product', (req, res) =>{
     const newProduct = {
-        id: products.id +1,
+        id:  products.length + 1,
         name: req.body.name,
         image: req.body.image,
         price: req.body.price,
@@ -35,6 +39,19 @@ app.post('/api/product', (req, res) =>{
     products.push(newProduct);
     res.json(newProduct)
 })
+
+app.put('/api/product/:id', (req, res) =>{
+ const productID = parseInt(req.body.id)
+ const singleProduct = products.find(products =>{products.id === productID}) 
+
+    products.name = req.body.name || product.name,
+    products.image = req.body.image || product.image,
+    products.price = req.body.price || product.price,
+    products.desc = req.body.desc || product.desc
+
+    res.json(singleProduct)
+
+});
 
 
 
@@ -47,3 +64,6 @@ app.get('*',(req, res)=>{
 app.listen(5000, ()=>{
     console.log(`http://localhost:${PORT}`);
 })
+
+
+
