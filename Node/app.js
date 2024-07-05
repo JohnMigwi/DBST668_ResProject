e60dbaefa6964d4e8be1a8d9854767a7a17f0702
JohnMigwi <johnmigwi101@gmail.com>
2024-07-05 10:14:29 +0300

    looger approach

--- Node/app.js
+++ Node/app.js
@@ -1,23 +1,16 @@
-const express = require('express');
+const express = require ('express')
+const loggering = require('../Node/logger')
 
-const app = express()
+const app = express();
 
-//req middleware res
-const logger = (req, res, next) =>{
-    const method = req.method;
-    const url = req.url;
-    const time = new Date().getFullYear();
-    console.log(method, url, time);
-    next()
-}
+app.use('/api', loggering)
 
-app.get('/', logger,(req, res)=>{
-   
-    res.send("Home")
+app.get('/', (req, res)=>{
+   res.send("Home")
 })
 
-app.get('/about', logger, (req, res) =>{
-    res.send("About")
+app.get('/product', (req, res) =>{
+   res.send("Products");
 })
 
-app.listen(5000, ()=>console.log(`https:localhost:5000`))
+app.listen(5000, ()=>console.log("logged AND STARTED"))
